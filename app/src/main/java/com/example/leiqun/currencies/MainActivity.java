64f31346e544be366e9e57146e802ebf2a3da2e6
qunleinature <leiqun.nature.mail@gmail.com>
2016-05-30 00:42:07 +0800

    Unpack currency codes from Bundle

--- app/src/main/java/com/example/leiqun/currencies/MainActivity.java
+++ app/src/main/java/com/example/leiqun/currencies/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.leiqun.currencies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -13,12 +10,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mCalcButton;
     private TextView mConvertedTextView;
     private EditText mAmountEditText;
     private Spinner mForSpinner,mHomSpinner;
+    private String[] mCurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //unPack ArrayList from the bundle and convert array
+        ArrayList<String> arrayList = (ArrayList<String>) getIntent().getSerializableExtra(SplashActivity.KEY_ARRAYLIST);
+        Collections.sort(arrayList);
+        mCurrencies = arrayList.toArray(new String[arrayList.size()]);
 
         //assign references to cur Views
         mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
