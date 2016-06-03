0bb5f47e62666d47d1519f9e8bbd6e2ccdeaf61c
qunleinature <leiqun.nature.mail@gmail.com>
2016-06-03 17:29:58 +0800

    修复 “Fires-up MainActivity with Intent and passes ArrayList into Bundle”提交 MainActivity崩溃 改MainActivity继承于AppCompatActivity，为继承于ActionBarActivity

--- app/src/main/java/com/example/leiqun/currencies/MainActivity.java
+++ app/src/main/java/com/example/leiqun/currencies/MainActivity.java
@@ -3,20 +3,19 @@ package com.example.leiqun.currencies;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
     @Override
