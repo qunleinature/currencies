5a80e8984130fb60aef82229caab457f095858c1
qunleinature <leiqun.nature.mail@gmail.com>
2016-06-04 18:00:41 +0800

    Creates find position given code method

--- app/src/main/java/com/example/leiqun/currencies/MainActivity.java
+++ app/src/main/java/com/example/leiqun/currencies/MainActivity.java
@@ -6,10 +6,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -66,6 +63,17 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
 
         mHomSpinner.setOnItemSelectedListener(this);
         mForSpinner.setOnItemSelectedListener(this);
+    }
+
+    private int findPositionGivenCode(String code,String[] currencies) {
+        for (int i=0;i< currencies.length;i++)
+        {
+            if (currencies[i].substring(0,3).equalsIgnoreCase(code)) {
+                return  i;
+            }
+        }
+
+        return  0;
     }
 
     public boolean isOnline() {
